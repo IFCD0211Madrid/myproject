51540c43e6fef25286ba0fcee18ba268a92c72d5
IFCD0211Madrid <IFCD0211Madrid@gmail.com>
2026-09-09 13:57:50 +0200

    Fin del día 9/9/26

--- name-initials/main.py
+++ name-initials/main.py
@@ -1,5 +1,17 @@
 def run(fullname: str) -> str:
     # TODO
+    nombre_completo =  []
+    nombre = fullname.split(',')[1].strip()
+    apellidos = fullname.split(',')[0].split()
+    nombre_completo.append(nombre)
+    #nombre_completo.append(apellidos[0])
+    #if len(apellidos) > 1:
+    #    nombre_completo.append(apellidos[1])
+    for apellido in apellidos:
+        nombre_completo.append(apellido)
+    #
+    iniciales=[cadena[0] for cadena in nombre_completo]
+    initials = '.'.join(iniciales).upper() + '.'
     return initials
 
 
